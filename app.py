20066c836a50892242be2ccbcdd53b390235f5a1
Chaitanyareddy0702 <chaitanyareddy0702@gmail.com>
2024-12-25 13:15:28 +0530

    Change File name and API KEY method

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import google.generativeai as genai
 
 
 
-genai.configure(api_key="YOUR_API_KEY")
+genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 model = genai.GenerativeModel('gemini-1.5-pro-latest')
 
 # Function to extract text from PDFs
